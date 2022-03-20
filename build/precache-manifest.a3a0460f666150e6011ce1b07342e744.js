eb623986806797c9dd4cd8a1f0824c5fd7c457ab
xuechengjinxiu <1034206061@qq.com>
2022-03-20 10:33:56 +0800

    favorite

---
+++ build/precache-manifest.a3a0460f666150e6011ce1b07342e744.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f02381d44bcb32dca3e6",
+    "url": "./static/js/main.f02381d4.chunk.js"
+  },
+  {
+    "revision": "343df934504d3090a7e5",
+    "url": "./static/js/1.343df934.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "4a289cfe37fba196b7cb0a2a54270afd",
+    "url": "./static/media/logo-black.4a289cfe.png"
+  },
+  {
+    "revision": "0ddcc773d374a4cee7bfca2fe73a9517",
+    "url": "./index.html"
+  }
+];
